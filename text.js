0b5bb14fb67201ae0f11b72019aafd71e2879840
USER <user@USERs-MacBook-Pro.local>
2023-03-02 06:26:15 -0800

    first commit

---
+++ text.js
@@ -0,0 +1,40 @@
+// Math method is an intrisic object that provide basic mathematics functionality and constant.
+
+// let x = 3.19;
+
+// x = Math.round(x)
+// console.log(x)
+
+// x = Math.floor(x)
+// console.log(x)
+
+// x = Math.ceil(x)
+// console.log(x)
+
+// x = Math.pow(x, 2)
+// console.log(x)
+
+// x = Math.sqrt(x, 4)
+// console.log(x)
+
+// Absolute neglect Minus(-)
+// x = Math.abs(x, 4)
+// console.log(x)
+
+// let x = 3.19;
+// let y = 5;
+// let z = 9
+
+//     maximum = Math.max (x, y, z);
+//     console.log(maximum)
+
+//     minimum = Math.min(x, y, z);
+//     console.log(minimum)
+
+
+
+// window interface are use to talk to the browser. the Dom is a property of window
+
+// const resetBtn = document.querySelector('#resetBtn');
+// resetBtn.addEventListener('click', () => window.print())
+
